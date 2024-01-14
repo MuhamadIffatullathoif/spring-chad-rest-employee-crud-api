fc3cbfe803c8291089fb15d8464bfcceecf3b899
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-14 09:40:25 +0700

    04. Spring Boot DAO Add, Update Delete

--- src/main/java/com/iffat/springchadrestemployeecrudapi/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/iffat/springchadrestemployeecrudapi/dao/EmployeeDAOJpaImpl.java
@@ -26,4 +26,23 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
         List<Employee> employees = theQuery.getResultList();
         return employees;
     }
+
+    @Override
+    public Employee findById(int id) {
+        Employee employee = entityManager.find(Employee.class, id);
+        return employee;
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        Employee dbEmployee = entityManager.merge(employee);
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Employee employee = entityManager.find(Employee.class, id);
+
+        entityManager.remove(employee);
+    }
 }
